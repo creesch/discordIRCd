733ba7d484351972ae38587896c67b0c6aa15c75
creesch <creesch.r@gmail.com>
2017-03-20 21:21:35 +0100

    Make channel mentions actually work...

--- server.js
+++ server.js
@@ -134,11 +134,11 @@ function parseDiscordLine(line, discordID) {
     }
 
     // Channels are also a thing!. 
-    const mentionChannelRegex = /(<@#\d{1,}?>)/g;
+    const mentionChannelRegex = /(<#\d{1,}?>)/g;
     const mentionChannelFound = line.match(mentionChannelRegex);
     if (mentionChannelFound) {
         mentionChannelFound.forEach(function(mention) {
-            const channelID = mention.replace(/<@#(\d{1,}?)>/, '$1');
+            const channelID = mention.replace(/<#(\d{1,}?)>/, '$1');
             const channelObject = discordClient.guilds.get(discordID).channels.get(channelID);    
 
             const replaceRegex = new RegExp(mention, 'g');
