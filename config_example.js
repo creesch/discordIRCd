69402e107fd99507575c7d54f0d61c496d06d89c
creesch <creesch@users.noreply.github.com>
2017-03-21 16:15:27 +0100

    Update config example

--- config_example.js
+++ config_example.js
@@ -1,5 +1,6 @@
 global.configuration = {
     DEBUG: false,
+    showOfflineUsers: true, // When true all users will always be shown. Offline users will be shown as away on clients that support away-notify.
     discordToken: '<TOKEN>',
     ircServer: {
         listenPort: 6667,
