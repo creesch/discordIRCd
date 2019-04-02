d30259e351bf7ce00dee8006cac1dd1e0643249f
Rodrigo Leite <rodrigolf.dev@gmail.com>
2019-04-01 22:52:56 -0300

    Initial implementation of the Discord service

---
+++ src/util/parseMessage.ts
@@ -0,0 +1,48 @@
+// Function that parses irc messages.
+// Shamelessly stolen from node-irc https://github.com/aredridel/node-ircd
+export function parseMessage(line) {
+    let message = {};
+    let m = /(:[^ ]+ )?([A-Z0-9]+)(?: (.*))?/i.exec(line);
+    if (!m) {
+        message['error'] = 'Unable to parse message';
+    } else {
+        let i;
+        if (m[3] && (i = m[3].indexOf(':')) !== -1) {
+            let rest = m[3].slice(i + 1);
+            message.params = m[3].slice(0, i - 1).split(' ');
+            message.params.push(rest);
+        } else {
+            if (m[3]) {
+                message.params = m[3].split(' ');
+            } else {
+                message.params = [];
+            }
+        }
+        if (m[2]) {
+            message.command = m[2].toUpperCase();
+        }
+        if (m[1]) {
+            message.sender = m[1];
+        }
+    }
+    return message;
+}
+
+// Returns a number based on the discord server that increases per call.
+// Used to make fairly sure nicknames on irc end up being unique after being scrubbed.
+// Make nicknames work for irc.
+function ircNickname(discordDisplayName: string, botuser: boolean, discriminator: string | number) {
+    const replaceRegex = /[^a-zA-Z0-9_\\[\]\{\}\^`\|]/g;
+    const shortenRegex = /_+/g;
+
+    if (replaceRegex.test(discordDisplayName)) {
+
+        let newDisplayname = `${discordDisplayName.replace(replaceRegex, '_')}${discriminator}`;
+        newDisplayname = newDisplayname.replace(shortenRegex, '_');
+
+        return botuser ? `${newDisplayname}[BOT]` : newDisplayname;
+
+    } else {
+        return botuser ? `${discordDisplayName}[BOT]` : discordDisplayName;
+    }
+}
